a8a6ec9a0be634e0b62fef175aad02a9b272e829
MIGUEL ANGEL MAMANI FLORES <64507341+miguelangelmamaniflores@users.noreply.github.com>
2025-12-10 23:55:34 -0500

    Remove MovimientoProductoMesActual interface

--- src/main/java/com/inventario/spring/app/model/MovimientoProductoMesActual.java
+++ src/main/java/com/inventario/spring/app/model/MovimientoProductoMesActual.java
@@ -1,9 +1,1 @@
-package com.inventario.spring.app.model;
 
-public interface MovimientoProductoMesActual {
-    Long getIdProducto();
-    String getNombreProducto();
-    Integer getCantidadTotal();
-    Integer getMes();
-    Integer getAnio();
-}
