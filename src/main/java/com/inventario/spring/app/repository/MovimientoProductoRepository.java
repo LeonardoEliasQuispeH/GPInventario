7ed2abc00e1c25fe7b88bbffe429e20d29c4eb64
LeonardoEliasQuispeH <147902081+LeonardoEliasQuispeH@users.noreply.github.com>
2025-12-16 00:09:46 -0500

    Update MovimientoProductoRepository.java

--- src/main/java/com/inventario/spring/app/repository/MovimientoProductoRepository.java
+++ src/main/java/com/inventario/spring/app/repository/MovimientoProductoRepository.java
@@ -61,5 +61,40 @@ public interface MovimientoProductoRepository extends JpaRepository<MovimientoPr
     @Query("SELECT m FROM MovimientoProducto m WHERE m.producto.id = :idProducto ORDER BY m.fechaMovimiento DESC")
     List<MovimientoProducto> findByIdProductoOrderByFechaDesc(@Param("idProducto") Long idProducto);
 
+    //ENTRADAS DEL MES ACTUAL
+    @Query(value = """
+    SELECT 
+        p.id AS idProducto,
+        p.nombre_producto AS nombreProducto,
+        SUM(m.cantidad_movimiento) AS cantidadTotal,
+        MONTH(m.fecha_movimiento) AS mes,
+        YEAR(m.fecha_movimiento) AS anio
+    FROM movimiento_producto m
+    INNER JOIN producto p ON p.id = m.id_producto
+    WHERE MONTH(m.fecha_movimiento) = MONTH(CURRENT_DATE())
+      AND YEAR(m.fecha_movimiento) = YEAR(CURRENT_DATE())
+      AND m.tipo_movimiento = 'entrada'
+    GROUP BY p.id, p.nombre_producto, mes, anio
+    """, nativeQuery = true)
+    List<MovimientoProductoMesActual> obtenerEntradasMesActual();
+
+    @Query(value = """
+    SELECT 
+        p.id AS idProducto,
+        p.nombre_producto AS nombreProducto,
+        SUM(m.cantidad_movimiento) AS cantidadTotal,
+        MONTH(m.fecha_movimiento) AS mes,
+        YEAR(m.fecha_movimiento) AS anio
+    FROM movimiento_producto m
+    INNER JOIN producto p ON p.id = m.id_producto
+    WHERE m.tipo_movimiento = 'entrada'
+      AND m.fecha_movimiento BETWEEN :inicio AND :fin
+    GROUP BY p.id, p.nombre_producto, MONTH(m.fecha_movimiento), YEAR(m.fecha_movimiento)
+    ORDER BY YEAR(m.fecha_movimiento), MONTH(m.fecha_movimiento), p.nombre_producto
+    """, nativeQuery = true)
+    List<MovimientoProductoMesActual> obtenerEntradasPorRango(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fin") LocalDateTime fin
+    );
     
 }
